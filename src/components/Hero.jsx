7b5e0daff7ce478817f0d7835a18f690020a29ff
JaySinghvi <98965593+JaySinghvi@users.noreply.github.com>
2024-08-08 22:51:23 +0530

    Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,14 +15,14 @@ const Hero = () => {
                         whileInView={{x:0, opacity:1}}
                         initial={{x:-100, opacity:0}}
                         transition={{duration:0.5, delay:0}}
-                        className='pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl'>
+                        className='pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-2xl'>
                         Jay Singhvi
                     </motion.h1>
                     <motion.span 
                         whileInView={{x:0, opacity:1}}
                         initial={{x:-100, opacity:0}}
                         transition={{duration:0.5, delay:0.5}}
-                        className='bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent pb-2'>
+                        className='bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-2xl tracking-tight text-transparent pb-2'>
                         Upcoming Data Scientist
                     </motion.span>
                     <motion.p

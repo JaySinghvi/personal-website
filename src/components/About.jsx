35498f2b9497a757f2a95ff9bf6cfde0fab01c97
JaySinghvi <98965593+JaySinghvi@users.noreply.github.com>
2025-04-08 12:28:03 -0400

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -42,7 +42,7 @@ const AlignTimeline = ({ align }) => (
 const About = () => {
   return (
     <div className='border-b border-neutral-900 pb-4 shadow-purple-900'>
-        <motion.h2 whileInView={{opacity:1, y:0}} initial={{opacity:0, y:-100}} transition={{duration:1.5}} className='my-20 text-center text-4xl'>Educational Highlights</motion.h2>
+        <motion.h2 whileInView={{opacity:1, y:0}} initial={{opacity:0, y:-100}} transition={{duration:1.5}} className='my-20 text-center text-4xl'>Academic Milestones</motion.h2>
         <div className=' shadow-purple-900 sm:w-4/5 lg:w-1/2'>
             <motion.div whileInView={{opacity:1, x:0}} initial={{opacity:0, x:100}} transition={{duration: 0.5}} className='w-full lg:w-1/2 shadow-purple-900 '>
                     <Grid fluid>

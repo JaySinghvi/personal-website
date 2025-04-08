7d79f313b5e3d257bad81a9c362590525009ddac
JaySinghvi <98965593+JaySinghvi@users.noreply.github.com>
2025-04-08 11:33:10 -0400

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,7 +15,7 @@ const Hero = () => {
                         initial={{x:-100, opacity:0}}
                         transition={{duration:0.5, delay:0}}
                         className='pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl'>
-                        Jay Singhvi
+                        Hi I'm Jay Singhvi 👋
                     </motion.h1>
                     <motion.p
                         whileInView={{x:0, opacity:1}}
@@ -23,8 +23,8 @@ const Hero = () => {
                         transition={{duration:0.5, delay:1}}
                         className='my-2 max-w-xl py-6 text-2xl'>
                         <ul>
-                          <li>🔭 I build intelligent systems that merge data, design, and business — from disaster response platforms to emotion-aware music recommenders</li>
-                          <li>🌱 Currently exploring deep & reinforcement learning, cloud deployment, and real-time data products</li>
+                          <li>🔭 I build intelligent systems that blend data, design, and real-world impact</li>
+                          <li>🌱 Exploring multi-modal learning, cloud deployment, and real-time AI applications</li>
                           <li>👨‍💻 Experienced in Python, R, SQL, TensorFlow, Scikit-learn, OpenCV, Streamlit, Power BI, and full-stack development</li>
                           <li>🚀 Passionate about solving real-world problems through NLP, recommender systems, predictive modeling, and automation</li>
                           <li>🎓 Studying Informatics & Data Science at UMass Amherst (GPA: 3.95), with a research background in secure medical AI</li>
@@ -34,7 +34,7 @@ const Hero = () => {
                     </motion.p>
                 </div>
             </div>
-            <div className='lg:w-1/2 lg:p-8'>
+            <div className='lg:w-1/4 lg:p-8'>
                 <div className='flex justify-center'>
                     <motion.img whileInView={{x:0, opacity:1}} initial={{x:100, opacity:0}} transition={{duration:1, delay:1.2}} className='rounded-3xl shadow-2xl shadow-purple-950' src={profilePic} alt="profile_pic"/>
                 </div>

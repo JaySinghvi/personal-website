43b02c8cd5ce060341edc4338c77a02e4c4bf8c5
JaySinghvi <98965593+JaySinghvi@users.noreply.github.com>
2025-04-08 11:38:52 -0400

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -35,7 +35,7 @@ const Hero = () => {
                 </div>
             </div>
             <div className='lg:w-1/2 lg:p-8'>
-                <div className='flex justify-center'>
+                <div className='flex justify-end scale-60'>
                     <motion.img whileInView={{x:0, opacity:1}} initial={{x:100, opacity:0}} transition={{duration:1, delay:1.2}} className='rounded-3xl shadow-2xl shadow-purple-950' src={profilePic} alt="profile_pic"/>
                 </div>
             </div>

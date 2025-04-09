e2e3f8a9d93a0f40acc4b6e31ac1fabf166ef88c
JaySinghvi <98965593+JaySinghvi@users.noreply.github.com>
2025-04-09 14:24:50 -0400

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -14,7 +14,7 @@ const Projects = () => {
 
   return (    
     <div className='border-b border-neutral-900 pb-4'>
-        <motion.h2 whileInView={{opacity:1, y:0}} initial={{opacity:0, y:-100}} transition={{duration:1.5}} className='my-20 text-center text-4xl'>A Multisector Approach: Data Science Across All Sectors</motion.h2>
+        <motion.h2 whileInView={{opacity:1, y:0}} initial={{opacity:0, y:-100}} transition={{duration:1.5}} className='my-20 text-center text-4xl'>Machine Learning Across Sectors 🧠🔗</motion.h2>
         <div className='mb-8 flex flex-wrap lg:justify-center'>
             <motion.div whileInView={{opacity:1, x:0}} initial={{opacity:0, x:-100}} transition={{duration:1}} className='w-full lg:w-1/2 lg:p-8'>
                 <div className='flex items-center justify-center'>

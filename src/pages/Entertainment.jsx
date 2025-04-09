a5b1d02fea1b93cf3bba5a3a9c62e129756672ac
JaySinghvi <98965593+JaySinghvi@users.noreply.github.com>
2025-04-09 16:10:13 -0400

    Update Entertainment.jsx

--- src/pages/Entertainment.jsx
+++ src/pages/Entertainment.jsx
@@ -17,12 +17,11 @@ const Entertainment = () => {
                 whileTap={{ scale: 0.9 }} 
                 transition={{ type: "spring", stiffness: 400, damping: 17, duration: 1.5 }} 
                 className=" border-pink-500 bg-gradient-to-r from-pink-400 via-slate-200 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">
-                <a href="https://github.com/JaySinghvi/Content-Based-Movie-Recommender-System"><h6 className='mb-2 fon-semibold text-3xl pb-3'><b>AI-Driven Movie Recommendation System</b></h6></a>
+                <a href="https://github.com/JaySinghvi/Content-Based-Movie-Recommender-System"><h6 className='mb-2 fon-semibold text-3xl pb-3'><b>Intelligent Movie Recommendation System</b></h6></a>
               </motion.button>
               <ul className='list-disc pl-5 lg:justify-center text-2xl'>
-                <li className='mb-2'>Created a personalized movie recommender using <b>machine learning</b> and <b>NLP</b> to analyze and preprocess film metadata</li>
-                <li className='mb-2'><b>Vectorized</b> movie details with <b>Python</b>, <b>scikit-learn</b>, and <b>NLTK</b>, then applied <b>TF-IDF</b> and <b>cosine similarity</b> for ranking</li>
-                <li className='mb-2'>Developed an interactive <b>Streamlit</b> app, integrating the <b>TMDB API</b> to fetch real-time movie posters, and delivering a scalable approach to content-based <b>recommendation systems</b></li>
+                <li className='mb-2'>Designed a personalized movie recommendation engine that analyzes film metadata using natural language processing techniques, computes semantic similarity through vectorization, and uses cosine distance to rank content by contextual relevance with user inputs</li>
+                <li className='mb-2'>Built and deployed a user-friendly Streamlit interface that dynamically fetches real-time movie posters through external TMDB API integration, creating an engaging, scalable recommendation experience based on user preferences and content similarity</li>
               </ul>
               <div className='flex flex-wrap justify-center'>
                 <span className='mr-2 mt-2 rounded bg-neutral-900 px-2 py-1 text-2xl font-medium text-purple-500'>Artificial Intelligence</span>
@@ -45,12 +44,11 @@ const Entertainment = () => {
                 whileTap={{ scale: 0.9 }} 
                 transition={{ type: "spring", stiffness: 400, damping: 17, duration: 1.5 }} 
                 className=" border-pink-500 bg-gradient-to-r from-pink-400 via-slate-200 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">
-                <a href="https://github.com/JaySinghvi/Music-Recommender-Based-on-User-Setiments"><h6 className='mb-2 fon-semibold text-3xl pb-3'><b>AI-Powered Sentiment-Based Music Recommender</b></h6></a>
+                <a href="https://github.com/JaySinghvi/Music-Recommender-Based-on-User-Setiments"><h6 className='mb-2 fon-semibold text-3xl pb-3'><b>Personalized Music by Emotion</b></h6></a>
               </motion.button>
               <ul className='list-disc pl-5 lg:justify-center text-2xl'>
-                <li className='mb-2'>Developed a <b>facial</b> and <b>hand recognition</b> pipeline with <b>TensorFlow & Keras</b>, classifying user emotions for <b>sentiment-based</b> music suggestions from YouTube</li>
-                <li className='mb-2'>Engineered <b>computer vision</b> and <b>sentiment analysis</b> methods, experimenting with hand sign recognition to refine mood detection and enhance recommendation accuracy</li>
-                <li className='mb-2'>Designed an interactive <b>Streamlit UI</b>, enabling language/artist preferences and delivering real-time, emotion-driven song recommendations</li>
+                <li className='mb-2'>Designed a deep learning-based emotion recognition pipeline to classify user mood in real-time through facial expressions and hand gestures, enabling intelligent music recommendations aligned with affective behavioral signals and emotional state analysis</li>
+                <li className='mb-2'>Converted the backend into a fully interactive Streamlit application, integrating third-party APIs to serve dynamic song suggestions and allowing users to personalize their experience through real-time selection of preferred artists, genres, and language settings</li>
               </ul>
               <div className='flex flex-wrap justify-center'>
                 <span className='mr-2 mt-2 rounded bg-neutral-900 px-2 py-1 text-2xl font-medium text-purple-500'>Facial and Hand Recognition</span>  

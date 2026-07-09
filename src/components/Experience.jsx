f1f81f389599af4b25ad80d4c0a9d38160b390cd
JaySinghvi <98965593+JaySinghvi@users.noreply.github.com>
2026-07-09 15:55:28 -0400

    Update experience details and internship positions

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -5,34 +5,7 @@ const Experience = () => {
   return (
         <div className='border-b border-neutral-900 pb-4'>
             <motion.h2 whileInView={{opacity:1, y:0}} initial={{opacity:0, y:-100}} transition={{duration:1.5}} className='my-20 text-center text-4xl'>Professional Journey 👨‍💼</motion.h2>
-            <div className='flex justify-center'>
-                <div className='mb-8 flex lg:justify-center'>
-                    <motion.div whileInView={{opacity:1, x:0}} initial={{opacity:0, x:-100}} transition={{duration:1}} className='w-full lg:w-1/4 justify-center items-center '>
-                        <p className='mb-2 text-violet-200 text-2xl'>May 2024 - August 2024</p>
-                    </motion.div>
-                    <motion.div whileInView={{opacity:1, x:0}} initial={{opacity:0, x:100}} transition={{duration:1}} className='w-full lg:pl-1/2 lg:w-3/4'>
-                        <motion.button 
-                            whileHover={{ scale: 1.2 }}
-                            whileTap={{ scale: 0.9 }} 
-                            transition={{ type: "spring", stiffness: 400, damping: 17, duration: 1.5 }} 
-                            className=" border-pink-500 bg-gradient-to-r from-pink-400 via-slate-200 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">
-                            <a href="https://app.moonshotpirates.com/voting/climate-24?team=1715422576809x820019839363973100"><h2 className='mb-2 fon-semibold text-4xl pb-3'>Machine Learning Engineer Intern - <span className='text-2xl text-white'>Moonshot Pirates</span></h2></a>
-                        </motion.button>
-                        <ul className='mb-3 text-2xl list-disc pl-5 lg:justify-center '>
-                            <li className='mb-2'>Developed a disaster response system that <b>reduced emergency response time by 30%</b> and <b>improved aid accuracy by 25%</b>, combining geospatial modeling, real-time impact forecasting, and resource allocation into a cohesive platform for emergency teams</li>
-                            <li className='mb-2'>Created and deployed an interactive dashboard that <b>visualized over 1,000 disaster scenarios</b>, enabling faster, data-backed decisions and <b>improving emergency coordination by 40%</b> through real-time risk mapping, geospatial overlays, and alert-based prioritization</li>
-                        </ul>
-                        <div className='flex flex-wrap'>
-                            <span className='mr-2 mt-2 rounded bg-neutral-900 px-2 py-1 text-2xl font-medium text-purple-500'>Predictive Modeling</span>
-                            <span className='mr-2 mt-2 rounded bg-neutral-900 px-2 py-1 text-2xl font-medium text-purple-500'>Simulation & Risk Mapping</span>
-                            <span className='mr-2 mt-2 rounded bg-neutral-900 px-2 py-1 text-2xl font-medium text-purple-500'>Data Visualization</span>
-                            <span className='mr-2 mt-2 rounded bg-neutral-900 px-2 py-1 text-2xl font-medium text-purple-500'>Dashboard Development</span>
-                        </div>            
-
-                    </motion.div>
-                </div>
-            </div>
-            <div className='flex justify-center'>
+              <div className='flex justify-center'>
                 <div className='mb-8 flex lg:justify-center'>
                     <motion.div whileInView={{opacity:1, x:0}} initial={{opacity:0, x:-100}} transition={{duration:1}} className='w-full lg:w-1/4 justify-center items-center '>
                         <p className='mb-2 text-violet-200 text-2xl'>November 2023 - January 2024</p>
@@ -58,6 +31,35 @@ const Experience = () => {
                     </motion.div>
                 </div>
             </div>
+          
+          <div className='flex justify-center'>
+                <div className='mb-8 flex lg:justify-center'>
+                    <motion.div whileInView={{opacity:1, x:0}} initial={{opacity:0, x:-100}} transition={{duration:1}} className='w-full lg:w-1/4 justify-center items-center '>
+                        <p className='mb-2 text-violet-200 text-2xl'>May 2025 - August 2025</p>
+                    </motion.div>
+                    <motion.div whileInView={{opacity:1, x:0}} initial={{opacity:0, x:100}} transition={{duration:1}} className='w-full lg:pl-1/2 lg:w-3/4'>
+                        <motion.button 
+                            whileHover={{ scale: 1.2 }}
+                            whileTap={{ scale: 0.9 }} 
+                            transition={{ type: "spring", stiffness: 400, damping: 17, duration: 1.5 }} 
+                            className=" border-pink-500 bg-gradient-to-r from-pink-400 via-slate-200 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">
+                            <a href="https://app.moonshotpirates.com/voting/climate-24?team=1715422576809x820019839363973100"><h2 className='mb-2 fon-semibold text-4xl pb-3'>Machine Learning Engineer Intern - <span className='text-2xl text-white'>Moonshot Pirates</span></h2></a>
+                        </motion.button>
+                        <ul className='mb-3 text-2xl list-disc pl-5 lg:justify-center '>
+                            <li className='mb-2'>Developed a disaster response system that <b>reduced emergency response time by 30%</b> and <b>improved aid accuracy by 25%</b>, combining geospatial modeling, real-time impact forecasting, and resource allocation into a cohesive platform for emergency teams</li>
+                            <li className='mb-2'>Created and deployed an interactive dashboard that <b>visualized over 1,000 disaster scenarios</b>, enabling faster, data-backed decisions and <b>improving emergency coordination by 40%</b> through real-time risk mapping, geospatial overlays, and alert-based prioritization</li>
+                        </ul>
+                        <div className='flex flex-wrap'>
+                            <span className='mr-2 mt-2 rounded bg-neutral-900 px-2 py-1 text-2xl font-medium text-purple-500'>Predictive Modeling</span>
+                            <span className='mr-2 mt-2 rounded bg-neutral-900 px-2 py-1 text-2xl font-medium text-purple-500'>Simulation & Risk Mapping</span>
+                            <span className='mr-2 mt-2 rounded bg-neutral-900 px-2 py-1 text-2xl font-medium text-purple-500'>Data Visualization</span>
+                            <span className='mr-2 mt-2 rounded bg-neutral-900 px-2 py-1 text-2xl font-medium text-purple-500'>Dashboard Development</span>
+                        </div>            
+
+                    </motion.div>
+                </div>
+            </div>
+            
             <div className='flex justify-center'>
                 <div className='mb-8 flex lg:justify-center'>
                     <motion.div whileInView={{opacity:1, x:0}} initial={{opacity:0, x:-100}} transition={{duration:1}} className='w-full lg:w-1/4 justify-center items-center '>
